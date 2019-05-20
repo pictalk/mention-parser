2bb3016864c363d786ebc0da9733b87ea1b0b7c3
Alex Regan <alex.joseph.regan@gmail.com>
2019-05-20 16:43:43 -0700

    fix: unsafe type constraints
    - bypass TS and trust our tests

--- src/parser.ts
+++ src/parser.ts
@@ -197,7 +197,7 @@ export function parse<T extends string = string>(
     tags.push({ type, value });
   }
 
-  return Ok(tags);
+  return Ok(tags) as any;
 }
 
 function expectNever(_: never, msg = "unexpected code path"): never {
